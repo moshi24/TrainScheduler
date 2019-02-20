9c5685bb219848aabe313585cbed4e77091bc34e
Kawah Wong <kawah.wong.w@gmail.com>
2019-02-19 23:23:56 -0800

    final attempt

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 // Initialize Firebase
+$(document).ready(function(){
+
 var config = {
     apiKey: "AIzaSyDmIwmr7UtcBXdGur7RWvaZrlLA5tP31sg",
     authDomain: "trainschedule-3a7d7.firebaseapp.com",
@@ -9,29 +11,81 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  var dataRef = firebase.database();
-  
-  var trainName = "";
-  var destination = "";
-  var trainTime = 0;
-  var frequency = 0;
- 
+  var database = firebase.database();
 
-  $("#submit").on("click", function(event) {
-    event.preventDefault();
-    
-    trainName = $("#name").val().trim();
-    destination = $("#destination").val().trim();
-    trainTime = $("#time").val().trim();
-    frequency = $("#frequency").val().trim();
+$("#submit").on("click", function() {
 
-    dataRef.ref().push({
 
-      trainName: trainName,
-      destination: destination,
-      trainTime: trainTime,
-      frequency: frequency,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-  });
+	var name = $('#nameInput').val().trim();
+    var dest = $('#destInput').val().trim();
+    var time = $('#timeInput').val().trim();
+    var freq = $('#freqInput').val().trim();
 
+
+	database.ref().push({
+		name: name,
+		dest: dest,
+    	time: time,
+    	freq: freq,
+    	timeAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+	
+	$("input").val('');
+    return false;
+});
+
+
+database.ref().on("child_added", function(childSnapshot){
+	
+	var name = childSnapshot.val().name;
+	var dest = childSnapshot.val().dest;
+	var time = childSnapshot.val().time;
+	var freq = childSnapshot.val().freq;
+
+	console.log("Name: " + name);
+	console.log("Destination: " + dest);
+	console.log("Time: " + time);
+	console.log("Frequency: " + freq);
+	
+
+
+	var freq = parseInt(freq);
+	
+	var currentTime = moment();
+	console.log("CURRENT TIME: " + moment().format('HH:mm'));
+	
+	var dConverted = moment(childSnapshot.val().time, 'HH:mm').subtract(1, 'years');
+	console.log("DATE CONVERTED: " + dConverted);
+	var trainTime = moment(dConverted).format('HH:mm');
+	console.log("TRAIN TIME : " + trainTime);
+	
+	 
+	var tConverted = moment(trainTime, 'HH:mm').subtract(1, 'years');
+	var tDifference = moment().diff(moment(tConverted), 'minutes');
+	console.log("DIFFERENCE IN TIME: " + tDifference);
+	
+	var tRemainder = tDifference % freq;
+	console.log("TIME REMAINING: " + tRemainder);
+	
+	var minsAway = freq - tRemainder;
+	console.log("MINUTES UNTIL NEXT TRAIN: " + minsAway);
+	
+	var nextTrain = moment().add(minsAway, 'minutes');
+	console.log("ARRIVAL TIME: " + moment(nextTrain).format('HH:mm A'));
+	
+$('#currentTime').text(currentTime);
+$('#trainTable').append(
+		"<tr><td id='nameDisplay'>" + childSnapshot.val().name +
+		"</td><td id='destDisplay'>" + childSnapshot.val().dest +
+		"</td><td id='freqDisplay'>" + childSnapshot.val().freq +
+		"</td><td id='nextDisplay'>" + moment(nextTrain).format("HH:mm") +
+		"</td><td id='awayDisplay'>" + minsAway  + ' minutes until arrival' + "</td></tr>");
+ },
+
+function(errorObject){
+    console.log("Read failed: " + errorObject.code)
+});
+
+
+
+}); 
